032cabf2e746bc1fa0f8b48b58de5e706b0fb4ce
jshaw <jshaw64@gmail.com>
2016-09-02 10:54:27 -0400

    Remove old config data

--- config.sh
+++ config.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
 CONF_FS=":"
-#
-CONF_KEY_DEBUG="debug"
-CONF_KEY_VERBOSE="verbose"
-CONF_KEY_COPY="copymode"
-CONF_KEY_LINK="linkmode"
-#
-CONF_KEY_FSRC="srcfile"
-CONF_KEY_FDST="dstfile"
-CONF_KEY_DROOT="dirroot"
-CONF_KEY_DGROUP="dirgroup"
-CONF_KEY_DARCH="dirarchive"
-#
-CONF_KEY_SCOPY="sync_copy"
-CONF_KEY_SLINK="sync_link"
-CONF_KEY_SGROUP="sync_group"
 
-CONFIG=(
-  "${CONF_KEY_FSRC}${CONF_FS}"
-  "${CONF_KEY_FDST}${CONF_FS}"
-  "${CONF_KEY_DROOT}${CONF_FS}"
-  "${CONF_KEY_DGROUP}${CONF_FS}"
-)
-
+CONFIG=()
 
 E_KEY=80
 
