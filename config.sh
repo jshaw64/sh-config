b5b942d2e8f97178e4bb24e4f07856c919aa2368
jshaw <jshaw64@gmail.com>
2016-09-28 20:33:47 -0400

    Fix spacing

--- config.sh
+++ config.sh
@@ -13,64 +13,60 @@ config_validate_key()
 
 config_print()
 {
-    echo "Config: Print: Active"
-
-    local i=0
-    for entry in "${CONFIG[@]}" ; do
-        local key=${entry%%:*}
-        local value=${entry#*:}
-        printf "\t[$i] Key: [$key] Value: [$value]\n"
-        (( i++ ))
-    done
+  local i=0
+  for entry in "${CONFIG[@]}" ; do
+      local key=${entry%%:*}
+      local value=${entry#*:}
+      printf "\t[$i] Key: [$key] Value: [$value]\n"
+      (( i++ ))
+  done
 }
 
 config_get()
 {
+  local get_key="$1"
+  local found=
 
-    local get_key="$1"
-    local found=
+  for entry in "${CONFIG[@]}" ; do
+      local key=${entry%%:*}
+      local value=${entry#*:}
+      if [ $key = $get_key ]; then
+          found="$value"
+          break
+      fi
+  done
 
-    for entry in "${CONFIG[@]}" ; do
-        local key=${entry%%:*}
-        local value=${entry#*:}
-        if [ $key = $get_key ]; then
-            found="$value"
-            break
-        fi
-    done
-
-    echo "$found"
+  echo "$found"
 }
 
 config_set()
 {
-    local set_key="$1"
-    local set_val="$2"
+  local set_key="$1"
+  local set_val="$2"
 
-    local i=0
-    local found=0
-    for entry in "${CONFIG[@]}" ; do
-        local key=${entry%%=*}
-        local value=${entry#*=}
-        if [ "$key" = "$set_key" ]; then
-            CONFIG[$i]="${key}${CONF_FS}${set_val}"
-            found=1
-            break
-        fi
-        (( i++ ))
-    done
+  local i=0
+  local found=0
+  for entry in "${CONFIG[@]}" ; do
+      local key=${entry%%=*}
+      local value=${entry#*=}
+      if [ "$key" = "$set_key" ]; then
+          CONFIG[$i]="${key}${CONF_FS}${set_val}"
+          found=1
+          break
+      fi
+      (( i++ ))
+  done
 
-    if [ $found -eq 0 ]; then
-        config_validate_key "$set_key"
-        local is_valid_key=$?
-        if [ $is_valid_key -gt 0 ]; then
-            echo "Error: invalid key [${set_key}]"
-            exit $E_KEY
-        fi
-        CONFIG=( "${CONFIG[@]}" "${set_key}${CONF_FS}${set_val}" )
-    fi
+  if [ $found -eq 0 ]; then
+      config_validate_key "$set_key"
+      local is_valid_key=$?
+      if [ $is_valid_key -gt 0 ]; then
+          echo "Error: invalid key [${set_key}]"
+          exit $E_KEY
+      fi
+      CONFIG=( "${CONFIG[@]}" "${set_key}${CONF_FS}${set_val}" )
+  fi
 }
-
 
 config_parse_file_block()
 {
